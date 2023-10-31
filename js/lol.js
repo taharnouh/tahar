b726927063a3234ef933214739a2deaf23e059f4
taharnouh <taharop@gmail.com>
2023-10-31 19:16:16 +0100

    menu and calculator

--- js/lol.js
+++ js/lol.js
@@ -1,38 +1,38 @@
- window.onload = (function () {
-    let bt = document.getElementById('bt');
-    var h = document.getElementById('h');
-    let a = document.getElementById("am");
-    var c = document.getElementById('c');
-    var j = document.getElementById('j');
-    var img = document.getElementById('im');
-    document.getElementById("competances").innerHTML = "My competances";
-    a.onmouseenter= function () {
-     img.src=a.src;   
+window.onload = function () {
+  let bt = document.getElementById("bt");
+  var h = document.getElementById("h");
+  let a = document.getElementById("am");
+  var c = document.getElementById("c");
+  var j = document.getElementById("j");
+  var img = document.getElementById("im");
+  document.getElementById("competances").innerHTML = "My competances";
+  a.onmouseenter = function () {
+    img.src = a.src;
+  };
+  h.onmouseenter = function () {
+    img.src = h.src;
+  };
+  c.onmouseenter = function () {
+    img.src = c.src;
+  };
+  j.onmouseenter = function () {
+    img.src = j.src;
+  };
+  function j() {
+    oro.style.display = "block";
+  }
+  bt.onclick = function () {
+    scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+  onscroll = function () {
+    if (scrollY >= 200) {
+      bt.style.display = "block";
+    } else {
+      bt.style.display = "none";
     }
-    h.onmouseenter=function () {
-        img.src=h.src;   
-       }
-    c.onmouseenter= function () {
-        img.src=c.src;   
-       }
-    j.onmouseenter=function () {
-        img.src=j.src;   
-       }
-      bt.onclick = function() {
-        
-        scroll({
-        top:0,
-        left:0,
-        behavior:"smooth",
-      })}
-       onscroll=function(){
-        if (scrollY>=200) {
-            bt.style.display= "block"
-        }
-        else{
-            bt.style.display= "none"  
-        }
-       }
-     })
-    
-    
+  };
+};
---
+++ js/menu.js
@@ -0,0 +1,21 @@
+window.onload = function (){
+    let menutxt = document.getElementById("menutxt");
+    let clbtn = document.getElementById("cl");
+    let op = document.getElementById("op");  
+    function d() {
+        clbtn.classList.add("hide")
+        menutxt.classList.add("hide")
+      }
+      d();
+      clbtn.onclick = function () {
+        this.classList.add("hide");
+        menutxt.classList.add("hide");
+        op.classList.remove("hide");
+      };
+      op.onclick = function () {
+        this.classList.add("hide");
+        menutxt.classList.remove("hide");
+        clbtn.classList.remove("hide");
+      };
+        
+};
