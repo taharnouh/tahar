71be99e3c097566e91ce9fe22c494ea01534cbec
taharnouh <taharop@gmail.com>
2024-06-24 22:23:01 +0100

    add the game

--- js/calc.js
+++ js/calc.js
@@ -5,6 +5,12 @@
     let st = document.getElementById("st");
     let sd = document.getElementById("sd");
     let res = document.getElementById("last"); 
+   const numbut = document.getElementById("num"); 
+   const oprebut = document.getElementById("opre");
+   const dlebut = document.getElementById("dle");
+   const rezalt = document.getElementById("rezalt");
+   const equle = document.getElementById("equle");
+
     plus.onclick=function(){
         res.value=( Number(st.value)+Number(sd.value)) ;    }
     min.onclick=function(){
@@ -13,3 +19,20 @@
         res.value=( Number(st.value)*Number(sd.value)) ;    }
     divid.onclick=function(){
         res.value=( Number(st.value)/Number(sd.value)) ;    }
+ console.log ("a");
+        function numclick() {
+            console.log ("b")
+numbut.onclick = function() {myFunction()};
+
+function myFunction() {
+ console.log("c");
+   this.rezalt = this.numbut;
+   console.log(this.rezalt);
+}
+}
+numclick ();
+
+
+
+
+
